2ae7e63ae779b179113b00678cd39df29136357c
Tim <tim.langner@tobit.com>
2020-11-12 16:32:27 +0100

    :lipstick: Always recenter img

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -22,8 +22,8 @@ const Header = () => {
         <>
             <div className="sticky">
                 <div className='header' style={ currentOffset < 55 ? { height: `${100 - currentOffset}px` } : { height: '45px' }}>
-                    <div className='image-wrapper' style={ currentOffset < 55 ? {top: `${5 - currentOffset / 1.1}%`} : {top: '-45%'}}>
-                        <img src={bamboo} alt="logo"style={ currentOffset < 55 ? {transform: `scale(${0.7 - ((currentOffset / 100) / 2.7)})`} : {transform: 'scale(0.5)'} }/>
+                    <div className='image-wrapper'>
+                        <img src={bamboo} alt="logo"/>
                     </div>
                 </div>
                 <Navbar />
