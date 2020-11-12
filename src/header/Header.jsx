f2c4ff9c0bd1b9cdd9c17ff3f3978c8cf57f4e8c
Tim <tim.langner@tobit.com>
2020-11-12 15:28:08 +0100

    :lipstick: recenter img based on pageYOffset

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -7,7 +7,6 @@ const Header = () => {
     const [headerOffset, setHeaderOffset] = useState(1);
     const [isSticky, setIsSticky] = useState(false);
     const [currentOffset, setCurrentOffset] = useState(0);
-    const imageRef = useRef(null);
 
     console.log(currentOffset);
 
@@ -23,8 +22,8 @@ const Header = () => {
         <>
             <div className="sticky">
                 <div className='header' style={ currentOffset <= 40 ? { height: `${100 - currentOffset}px` } : { height: '60px' }}>
-                    <div className='image-wrapper'>
-                        <img src={bamboo} alt="logo" ref={imageRef} style={ currentOffset < 40 ? {transform: `scale(${0.7 - ((currentOffset / 100) / 2)})`} : {transform: 'scale(0.5)'} }/>
+                    <div className='image-wrapper' style={ currentOffset < 40 ? {top: `${5 - currentOffset / 1.6}%`} : {top: '-20%'}}>
+                        <img src={bamboo} alt="logo"style={ currentOffset < 40 ? {transform: `scale(${0.7 - ((currentOffset / 100) / 2)})`} : {transform: 'scale(0.5)'} }/>
                     </div>
                 </div>
                 <Navbar />
