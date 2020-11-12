d3d5d44bdfe6321f357ace6358c82323076b0fe1
Tim <tim.langner@tobit.com>
2020-11-12 16:12:48 +0100

    :lipstick: Change header logo height after animation

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -8,6 +8,8 @@ const Header = () => {
     const [isSticky, setIsSticky] = useState(false);
     const [currentOffset, setCurrentOffset] = useState(0);
 
+    console.log(currentOffset);
+
     useEffect(() => {
         window.addEventListener('scroll', e => handleScroll(e));
     }, []);
@@ -19,9 +21,9 @@ const Header = () => {
     return (
         <>
             <div className="sticky">
-                <div className='header' style={ currentOffset <= 40 ? { height: `${100 - currentOffset}px` } : { height: '60px' }}>
-                    <div className='image-wrapper' style={ currentOffset < 40 ? {top: `${5 - currentOffset / 1.6}%`} : {top: '-20%'}}>
-                        <img src={bamboo} alt="logo"style={ currentOffset < 40 ? {transform: `scale(${0.7 - ((currentOffset / 100) / 2)})`} : {transform: 'scale(0.5)'} }/>
+                <div className='header' style={ currentOffset < 55 ? { height: `${100 - currentOffset}px` } : { height: '45px' }}>
+                    <div className='image-wrapper' style={ currentOffset < 55 ? {top: `${5 - currentOffset / 1.1}%`} : {top: '-45%'}}>
+                        <img src={bamboo} alt="logo"style={ currentOffset < 55 ? {transform: `scale(${0.7 - ((currentOffset / 100) / 2.7)})`} : {transform: 'scale(0.5)'} }/>
                     </div>
                 </div>
                 <Navbar />
