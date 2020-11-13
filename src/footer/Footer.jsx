3271d9c4c06e6d4ac067038aa81a1838d2fb78b2
Tim <tim.langner@tobit.com>
2020-11-13 08:58:07 +0100

    :construction: Add company info and pages to footer

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import chayns from '../images/chayns-logo-long.png';
+import bambooLogo from '../images/bambooLogo.png';
 import './footer.scss';
 
 const Footer = () => {
@@ -7,9 +8,18 @@ const Footer = () => {
         <div className="footer-wrapper">
             <div className="pages-footer">
                 <div className="company-info">
-                    <div className="logo">
-
+                    <div className="company-logo">
+                        <img src={bambooLogo} alt="bamboo-logo" />
                     </div>
+                    <div className="company-address">
+                        <p style={{ fontWeight: 'bold' }}>BamBoo! Ahaus</p>
+                        <p>Parallelstraße 46</p>
+                        <p>48683 Ahaus</p>
+                    </div>
+                </div>
+                <div className="pages">
+                    <p>Shutdown</p>
+                    <p>Gutscheine</p>
                 </div>
             </div>
             <div className="chayns-footer">
