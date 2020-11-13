2dc706134642f628633496b209a5e897ba96d493
Tim <tim.langner@tobit.com>
2020-11-13 10:00:01 +0100

    :recycle: Refactor

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -27,8 +27,8 @@ const Footer = () => {
                     <img src={chayns} alt="chayns-logo" />
                 </div>
                 <div className="privacy-wrapper">
-                    <a className="privacy-wrapper__item">Impressum</a>
-                    <a className="privacy-wrapper__item">Datenschutz</a>
+                    <a className="privacy-wrapper__item" href="javascript:void(0)">Impressum</a>
+                    <a className="privacy-wrapper__item" href="javascript:void(0)">Datenschutz</a>
                 </div>
             </div>
         </div>
